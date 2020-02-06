a7fc3d877b72cbe2a7ec5a6e32e2401cdb808e35
Alumne <Alumne@104-PC11>
2020-02-06 18:08:59 +0100

    cOMMITO

--- src/InsertDataDemo.java
+++ src/InsertDataDemo.java
@@ -9,9 +9,13 @@ public class InsertDataDemo {
 		Statement stmt = null;
 		
 		//Menu per demanar al usuari que vol fer
-		System.out.println("1= Inserir || 2= Modificar || 3= Esborrar");
+		System.out.println("1= Inserir alumne || 2= Modificar alumne || 3= Esborrar alumne");
 		Scanner teclado = new Scanner(System.in);
 		int menu = teclado.nextInt();
+		
+		//Menu per demanar al usuari que vol fer
+		System.out.println("1= Inserir poblacio || 2= Modificar poblacio || 3= Esborrar poblacio");
+		int menu2 = teclado.nextInt();
 		
 		try
 		{
@@ -23,7 +27,7 @@ public class InsertDataDemo {
 			
 			//Si vol inserir
 			if(menu == 1){
-				stmt.execute("INSERT INTO ALUMNES (NOM, DNI, DATA_NAIXEMENT, ADREÇA, CODI_POSTAL, POBLACIO) VALUES ('PEPE', '82984728P', '1992-12-21', 'C/ CIUTAT 3', '43840', 'TARRAGONA')");
+				stmt.execute("INSERT INTO ALUMNES (NOM, DNI, DATA_NAIXEMENT, ADREÇA, CODI_POSTAL) VALUES ('PEPE', '82984728P', '1992-12-21', 'C/ CIUTAT 3', '43840')");
 				System.out.println("Inserit correctament");
 			//Si vol modificar
 			}else if(menu == 2){
@@ -32,6 +36,23 @@ public class InsertDataDemo {
 			//Si vol eliminar
 			}else if(menu == 3){
 				stmt.execute("DELETE FROM ALUMNES WHERE DNI = '82984728P'");
+				System.out.println("Eliminat correctament");
+			//Si no entra al menu
+			}else{
+				System.out.println("error");
+			}
+			
+			//Si vol inserir
+			if(menu2 == 1){
+				stmt.execute("INSERT INTO POBLACIO (NOM, CODI_POSTAL) VALUES ('SALOU', '43840')");
+				System.out.println("Inserit correctament");
+			//Si vol modificar
+			}else if(menu2 == 2){
+				stmt.execute("SELECT NOM WHERE CODI_POSTAL = '43840'");
+				System.out.println("Llistat correctament");
+			//Si vol eliminar
+			}else if(menu2 == 3){
+				stmt.execute("DELETE FROM POBLACIO WHERE CODI_POSTAL = '43840'");
 				System.out.println("Eliminat correctament");
 			//Si no entra al menu
 			}else{
